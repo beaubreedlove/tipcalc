f92638c62c53940246f3a189179e7ad154c5c75b
Beau <beaubreedlove@gmail.com>
2015-08-24 23:17:05 -0400

    having trouble with NSUserDefaults

--- tipcalc/SettingsViewController.swift
+++ tipcalc/SettingsViewController.swift
@@ -9,11 +9,21 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var defaultTip1: UITextField!
+    @IBOutlet weak var defaultTip2: UITextField!
+    @IBOutlet weak var defaultTip3: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        // var defaults = NSUserDefaults.standardUserDefaults()
+        // defaultTip1.text = String(defaults.integerForKey("tip1"))
+        // defaultTip2.text = String(defaults.integerForKey("tip2"))
+        // defaultTip3.text = String(defaults.integerForKey("tip3"))
     }
 
     override func didReceiveMemoryWarning() {
